cc5ca26382399b46f0e12ec8c3bdf78660ca655d
Allotey Ngeke <angeke@umd.edu>
2023-04-17 20:59:25 -0400

    Added files

---
+++ mango.js
@@ -0,0 +1,193 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");   /* Accessing express module */
+const app = express();  /* app is a request handler function */
+const bodyParser = require("body-parser");
+const { render } = require("ejs");
+const statusCode = 200;
+const portNumber = 5000;
+
+require("dotenv").config({ path: path.resolve(__dirname, 'credentialsDontPost/.env') }) 
+
+const userName = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
+
+
+app.set("views", path.resolve(__dirname, "template"));
+app.set("view engine", "ejs");
+process.stdin.setEncoding("utf8");
+
+
+app.get("/", (request, response) => {
+
+    response.render("mango");
+
+});
+
+app.get("/account", (request, response) => {
+
+    response.render("account");
+
+});
+
+app.get("/olive", (request, response) => {
+
+    response.render("olive");
+
+});
+
+app.get("/cream", (request, response) => {
+
+    response.render("cream");
+
+});
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.post("/accountResult", (request, response) => {
+
+    const variables = { 
+       
+        email : request.body.email
+       
+       
+    };
+
+    console.log(request.body.email)
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    async function main() {
+        const uri = `mongodb+srv://${userName}:${password}@cluster0.laouepe.mongodb.net/?retryWrites=true&w=majority`
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+        try {
+            await client.connect();
+            let emailVal = request.body.email;
+            await lookUpOneEntry(client, databaseAndCollection, emailVal);
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+
+    }
+
+    async function lookUpOneEntry(client, databaseAndCollection, emailName) {
+        let filter = {email: emailName};
+        const result = await client.db(databaseAndCollection.db)
+                            .collection(databaseAndCollection.collection)
+                            .findOne(filter);
+    
+      
+        
+        
+        let resultEmail = ""
+        let resultName = ""
+        let resultColor = ""
+        if (result) {
+           
+            resultEmail = result.email;
+            resultName = result.name;
+            resultColor = result.color;
+            
+        } else {
+          
+            resultEmail = "None"
+            resultName = "None"
+            resultColor = "None"
+        }
+        
+        const variables = {
+              
+                email : resultEmail,
+                name : resultName,
+                color : resultColor
+            }
+            response.render("accountResult", variables);
+    }
+
+    main().catch(console.error);
+    
+
+});
+
+app.post("/creamConfirm", (request, response) => {
+
+    const variables = { 
+        name: request.body.name,
+        email : request.body.email,
+        address :request.body.address,
+        city: request.body.city,
+        state: request.body.state
+       
+       
+    };
+
+    response.render("creamConfirm", variables);
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    async function main() {
+        const uri = `mongodb+srv://${userName}:${password}@cluster0.laouepe.mongodb.net/?retryWrites=true&w=majority`
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        
+        try {
+            let apply = {name: request.body.name, color: "Cream", email: request.body.email,  address :request.body.address, city: request.body.city, state: request.body.state, nameCard: request.body.cardname, 
+                cardNumber: request.body.cardnumber, expMonth: request.body.expmonth, expYear: request.body.expyear, cvv: request.body.cvv  };
+
+            await insertApply(client, databaseAndCollection, apply);
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    }
+    async function insertApply(client, databaseAndCollection, newApply) {
+        const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(newApply);
+    
+        //console.log(`Apply entry created with id ${result.insertedId}`);
+    }
+    main().catch(console.error);
+});
+
+app.post("/oliveConfirm", (request, response) => {
+
+    const variables = { 
+        name: request.body.name,
+        email : request.body.email,
+        address :request.body.address,
+        city: request.body.city,
+        state: request.body.state
+       
+       
+    };
+
+    //console.log(name)
+    response.render("oliveConfirm", variables);
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    async function main() {
+        const uri = `mongodb+srv://${userName}:${password}@cluster0.laouepe.mongodb.net/?retryWrites=true&w=majority`
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        
+        try {
+            let apply = {name: request.body.name, color: "Olive", email: request.body.email,  address :request.body.address, city: request.body.city, state: request.body.state, nameCard: request.body.cardname, 
+                cardNumber: request.body.cardnumber, expMonth: request.body.expmonth, expYear: request.body.expyear, cvv: request.body.cvv  };
+
+            await insertApply(client, databaseAndCollection, apply);
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    }
+    async function insertApply(client, databaseAndCollection, newApply) {
+        const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(newApply);
+    
+        //console.log(`Apply entry created with id ${result.insertedId}`);
+    }
+    main().catch(console.error);
+});
+
+
+
+app.use( express.static( "public" ) );
+
+app.listen(portNumber);
